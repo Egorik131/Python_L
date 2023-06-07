2bfa7afbd32fb574320e2e9107090f4d6e134356
*Egor <egorik13@gmail.com>
2023-06-07 08:58:35 +0300

    hw_5_020623

---
+++ seminar/s_6_060623.py
@@ -0,0 +1,36 @@
+# def hypoth_count(a, b):
+#     return (a ** 2 + b ** 2) ** (1 / 2)
+#
+#
+# hyp = hypoth_count(3, 4)
+# print(hyp)
+
+# list_square_and_cube_root = list(map(lambda x: (x ** 2) ** (1 / 3), range(30)))
+# print(list_square_and_cube_root)
+
+# list_1 = [1, 2, 3, 4]
+# list_copy = list(map(lambda x: x, list_1))
+# print(list_copy)
+
+# @lopistopi
+
+# list_ = [1,2,3]
+# for elem in list_:
+#     print(elem)
+
+
+import math
+# ---orbits = [(1, 3), (2.5, 10), (7, 2), (6, 6), (4, 3)]
+# ---max_1 = 0
+
+# ---list_of_orbits = list(map(lambda ))
+# ---def find_farthest_orbit(list_of_orbits):
+# ---S = pi*a*b
+# ---list_of_orbits
+# ---print(*find_farthest_orbit(orbits))
+
+
+# orbits = [(1, 3), (2.5, 10), (7, 2), (6, 6), (4, 3)]
+# def find_orb(orbits):
+#     return max(orbits, key = lambda x: x[0]*x[1] if x[0] != x[1] else -1)
+# print(find_orb(orbits))
