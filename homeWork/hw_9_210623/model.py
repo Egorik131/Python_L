45c74b401b264c70a2df8fd485534d39dfb0b618
*Egor <egorik13@gmail.com>
2023-06-22 15:26:39 +0300

    hw_9_220623

---
+++ homeWork/hw_9_210623/model.py
@@ -0,0 +1,42 @@
+import json
+
+phone_book = {}
+path = 'phones.json'
+
+
+def open_file():
+    global phone_book
+    try:
+        with open(path, 'r', encoding='UTF-8') as file:
+            phone_book = json.load(file)
+        return True
+    except:
+        return False
+
+
+def save_file():
+    try:
+        with open(path, 'w', encoding='UTF-8') as file:
+            json.dump(phone_book, file, ensure_ascii=False)
+        return True
+    except:
+        return False
+
+
+def search(word: str) -> dict[int:dict[str, str]]:
+    result = {}
+    for index, contact in phone_book.items():
+        if word.lower() in ' '.join(contact.values()).lower():
+            result[index] = contact
+    return result
+
+
+def check_id():
+    if phone_book:
+        return max(phone_book) + 1
+    return 1
+
+
+def add_contact(new: {int: dict[str, str]}):
+    contact = {check_id(): new}
+    phone_book.update(contact)
